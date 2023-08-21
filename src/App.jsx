bb54d997e4fc209b077c0a7f1bbb35d508297e00
John <ashton.basile@gmail.com>
2023-08-21 11:24:10 +0100

    the increment and decrement functions now update the count correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+
+  const decrement = () => {
+    setCounter(counter - 1);
+  };
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>Counter: {counter}</p>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={increment}>Increment</button>
+    </>
+  );
 }
 
-export default App
+export default App;
